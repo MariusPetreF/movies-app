9f85d26aafc424d4edbb1d316f721141b662389c
MariusPetreF <mayutz96@gmail.com>
2024-01-03 21:15:22 +0200

    init

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,9 +5,9 @@ const Account = () => {
   return (
     <>
     <div className='w-full text-white'>
-      <img className='absolute w-full h-[400px] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c8686aa2-2b79-4fed-ba7e-1745e1517329/RO-ro-20231211-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" />
+      <img className='w-full h-[400px] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c8686aa2-2b79-4fed-ba7e-1745e1517329/RO-ro-20231211-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" />
       <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
-      <div className='absolute top-[20%] p-4 md:p-8'>
+      <div className='absolute top-[10%] p-4 md:p-8'>
         <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
       </div>
     </div>
